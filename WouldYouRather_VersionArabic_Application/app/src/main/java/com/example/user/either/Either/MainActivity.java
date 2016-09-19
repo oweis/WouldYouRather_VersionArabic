58b064f3866f27099d9ba3d82584dc9c50c70336
zolay96 <zouheirlen@gmail.com>
2016-09-19 21:37:54 +0200

    Update MainActivity.java

--- WouldYouRather_VersionArabic_Application/app/src/main/java/com/example/user/either/Either/MainActivity.java
+++ WouldYouRather_VersionArabic_Application/app/src/main/java/com/example/user/either/Either/MainActivity.java
@@ -5,14 +5,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.user.either.Data.PrefManager;
 import com.example.user.either.R;
 
 public class MainActivity extends AppCompatActivity {
-
+    
+    PrefManager manager;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+         manager = new PrefManager(this);
+        if(!manager.isLoggedIn()){
+            startActivity(new Intent(MainActivity.this, Login.class));
+        }
     }
 
     public void openPlay(View v) {
